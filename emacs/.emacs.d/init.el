832ce411e686511a8c03c54be21a43838e802092
Tim Welch <tim@welchnet.org>
2021-05-16 10:10:44 -0400

    feature: Switch to treemacs instead of neotree
    treemacs' interface is more intuitive to me.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -201,12 +201,102 @@
 
 
 ;; Neotree - file list window (like Nerdtree)
-(use-package neotree)
+;; (use-package neotree)
 
 ;; Treemacs - file list window
-;; (use-package lsp-treemacs)
-;; (use-package treemacs-evil)
-;; (use-package treemacs-projectile)
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-directory-name-transformer    #'identity
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-extension-regex          treemacs-last-period-regex-value
+          treemacs-file-follow-delay             0.2
+          treemacs-file-name-transformer         #'identity
+          treemacs-follow-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-move-forward-on-expand        nil
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-read-string-input             'from-child-frame
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-asc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-user-mode-line-format         nil
+          treemacs-user-header-line-format       nil
+          treemacs-width                         35
+          treemacs-workspace-switch-cleanup      nil)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after (treemacs dired)
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+(use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
+  :after (treemacs persp-mode) ;;or perspective vs. persp-mode
+  :ensure t
+  :config (treemacs-set-scope-type 'Perspectives))
 
 ;; Git management
 (use-package magit    ; git porcelain inside emacs
@@ -274,13 +364,13 @@
       ;; )
 
       `(("t" "Task" entry (file+olp+datetree "~/slip-box/agenda/inbox.org")
-	 "* TODO %?\n %U\n %i\n %a" :empty-lines 1)
+	 "* TODO %?\n %i\n %a" :empty-lines 1)
 	("p" "Project" entry (file+olp+datetree "~/slip-box/agenda/inbox.org")
-	 "* PROJ %?\n %U\n %i\n %a" :empty-lines 1)
+	 "* PROJ %?\n %i\n %a" :empty-lines 1)
 	("j" "Journal entry" entry (file+olp+datetree "~/slip-box/journal/journal.org")
 	 "* %U\n %?\n %i\n %a")
 	("n" "Note" entry (file+olp+datetree "~/slip-box/agenda/inbox.org")
-	 "* %U\n %?\n %i\n %a")
+	 "* %?\n %i\n %a")
 	)
       )
 	 
@@ -420,10 +510,10 @@
 
 
 ;; Key bindings
+(setq evil-want-keybinding nil) ; don't override map for dired and ibuffer
 (use-package evil       ; vi mode for emacs
   :init
   (setq evil-want-integration t) ; load evil-integration.el to setup vi key bindings
-  (setq evil-want-keybinding nil) ; don't override map for dired and ibuffer
   (setq evil-want-C-u-scroll t) ; use vi's C-u to scroll up instead of augmenting emacs commands
   (setq evil-want-C-i-jump t) ; jump to next item in list
   
