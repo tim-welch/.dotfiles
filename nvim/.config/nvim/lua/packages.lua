d9b6869f2013f71acf2fffd6b50ecb556051d387
Tim Welch <tim@welchnet.org>
2022-05-28 19:57:46 -0400

    Change welcome screen to dashboard

--- nvim/.config/nvim/lua/packages.lua
+++ nvim/.config/nvim/lua/packages.lua
@@ -11,11 +11,30 @@ return require('packer').startup(function(use)
 
     -- Put plugins here
     use { -- alpha-nvim replaces the default nvim dashboard
-        'goolord/alpha-nvim',
-        requires = { 'kyazdani42/nvim-web-devicons' },
+        "goolord/alpha-nvim",
         config = function ()
-            require'alpha'.setup(require'alpha.themes.startify'.opts)
-            vim.api.nvim_set_keymap('n', '<c-n>', ':Alpha<cr>', { noremap = true })
+            local alpha = require'alpha'
+            local dashboard = require'alpha.themes.dashboard'
+            dashboard.section.header.val = {
+                [[                               __                ]],
+                [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+                [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+                [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+                [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+                [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+            }
+            dashboard.section.buttons.val = {
+                dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
+                dashboard.button( "c", "  Configuration", ":e ~/.config/nvim/init.lua<CR>"),
+                dashboard.button( "q", "  Quit NVIM" , ":qa<CR>"),
+            }
+            local handle = io.popen('fortune')
+            local fortune = handle:read("*a")
+            handle:close()
+            dashboard.section.footer.val = fortune
+            dashboard.config.opts.noautocmd = true
+            vim.cmd[[autocmd User AlphaReady echo 'ready']]
+            alpha.setup(dashboard.config)
         end
     }
 
