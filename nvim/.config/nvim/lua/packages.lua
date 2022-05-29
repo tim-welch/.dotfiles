ecd035a07a4fd196dda780cae25fe4c163f73c5c
Tim Welch <tim@welchnet.org>
2022-05-29 14:55:54 -0400

    feat: make packer sync when config is saved

--- nvim/.config/nvim/lua/packages.lua
+++ nvim/.config/nvim/lua/packages.lua
@@ -4,13 +4,39 @@ local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
     -- If packer not installed, clone it before configuring
     packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    print "Installing packer; close and reopen neovim..."
 end
-return require('packer').startup(function(use)
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packages.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+return packer.startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
     -- Put plugins here
-    use { -- alpha-nvim replaces the default nvim dashboard
+    use {
+        -- replace the default nvim dashboard
         "goolord/alpha-nvim",
         config = function ()
             local alpha = require'alpha'
