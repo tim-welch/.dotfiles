a960c3dc0f1ed1f86b8ebaa7a7ac338b9b68c0f1
Tim Welch <tim@welchnet.org>
2022-05-30 20:26:54 -0400

    feat: configure diagnostic signs

--- nvim/.config/nvim/lua/my/lsp.lua
+++ nvim/.config/nvim/lua/my/lsp.lua
@@ -1,16 +1,16 @@
 local status_ok, lspconf = pcall(require, "lspconfig")
 if not status_ok then
-  return
+    return
 end
 
-local status_ok, cmplsp = pcall(require, "cmp_nvim_lsp")
-if not status_ok then
+local status_ok1, cmplsp = pcall(require, "cmp_nvim_lsp")
+if not status_ok1 then
     print("cmp-nvim-lsp is not installed")
     return
 end
 
-local status_ok, lspinst = pcall(require, "nvim-lsp-installer")
-if not status_ok then
+local status_ok2, lspinst = pcall(require, "nvim-lsp-installer")
+if not status_ok2 then
     print("nvim-lsp-installer is not installed")
     return
 end
@@ -25,13 +25,19 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = cmplsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local servers = { "clangd", }
 for _, lsp in ipairs(servers) do
-    lspconf[lsp].setup{
+    lspconf[lsp].setup {
         on_attach = on_attach,
         capabilities = capabilities
     }
+end
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 lspconf.sumneko_lua.setup {
@@ -42,8 +48,6 @@ lspconf.sumneko_lua.setup {
             runtime = {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = 'LuaJIT',
-                -- Setup your lua path
-                path = runtime_path,
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
