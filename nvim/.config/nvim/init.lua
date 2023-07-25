cd865f5795aba448d1ec4624692f8fd5a0ddcafd
Tim Welch <tim@welchnet.org>
2023-07-24 20:29:21 -0400

    nvim changes
    - update packages
    - add todo comments
    - cleanup formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,13 +18,21 @@ require('packer').startup(function(use)
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
 
-      -- Useful status updates for LSP
-      'j-hui/fidget.nvim',
-
       -- Additional lua configuration, makes nvim stuff amazing
       'folke/neodev.nvim',
     },
   }
+      -- Useful status updates for LSP
+  use {
+        'j-hui/fidget.nvim',
+        tag = 'legacy',
+    config = function()
+      require("fidget").setup {
+        -- options
+      }
+    end,
+  }
+
 
   use { -- Autocompletion
     'hrsh7th/nvim-cmp',
@@ -74,6 +82,78 @@ require('packer').startup(function(use)
 
   -- better undo
   use 'mbbill/undotree'
+
+  -- highlight and search todo comments
+  use {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+        signs = true, -- show icons in the signs column
+        sign_priority = 8, -- sign priority
+        -- keywords recognized as todo comments
+        keywords = {
+          FIX = {
+            icon = " ", -- icon used for the sign, and in search results
+            color = "error", -- can be a hex color, or a named color (see below)
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+          },
+          TODO = { icon = " ", color = "info" },
+          HACK = { icon = " ", color = "warning" },
+          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+          PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+          TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+        },
+        gui_style = {
+          fg = "NONE", -- The gui style to use for the fg highlight group.
+          bg = "BOLD", -- The gui style to use for the bg highlight group.
+        },
+        merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+        -- highlighting of the line containing the todo comment
+        -- * before: highlights before the keyword (typically comment characters)
+        -- * keyword: highlights of the keyword
+        -- * after: highlights after the keyword (todo text)
+        highlight = {
+          multiline = true, -- enable multine todo comments
+          multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+          multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+          before = "", -- "fg" or "bg" or empty
+          keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+          after = "fg", -- "fg" or "bg" or empty
+          pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
+          comments_only = true, -- uses treesitter to match keywords in comments only
+          max_line_len = 400, -- ignore lines longer than this
+          exclude = {}, -- list of file types to exclude highlighting
+        },
+        -- list of named colors where we try to extract the guifg from the
+        -- list of highlight groups or use the hex color if hl not found as a fallback
+        colors = {
+          error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+          warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+          info = { "DiagnosticInfo", "#2563EB" },
+          hint = { "DiagnosticHint", "#10B981" },
+          default = { "Identifier", "#7C3AED" },
+          test = { "Identifier", "#FF00FF" }
+        },
+        search = {
+          command = "rg",
+          args = {
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+          },
+          -- regex that will be used to match keywords.
+          -- don't replace the (KEYWORDS) placeholder
+          pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+          -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+        },
+      }
+    end
+  }
 
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   -- These are my own plugins
@@ -182,16 +262,16 @@ vim.o.clipboard = "unnamedplus" -- use the system clipboard unless a specific re
 if vim.env.TMUX then
   -- https://rumpelsepp.org/blog/nvim-clipboard-through-ssh/
   vim.g.clipboard = {
-      name = 'tmux',
-      copy = {
-          ["+"] = {'tmux', 'load-buffer', '-w', '-'},
-          ["*"] = {'tmux', 'load-buffer', '-w', '-'},
-      },
-      paste = {
-          ["+"] = {'bash', '-c', 'tmux refresh-client -l && sleep 0.2 && tmux save-buffer -'},
-          ["*"] = {'bash', '-c', 'tmux refresh-client -l && sleep 0.2 && tmux save-buffer -'},
-      },
-      cache_enabled = false,
+    name = 'tmux',
+    copy = {
+      ["+"] = { 'tmux', 'load-buffer', '-w', '-' },
+      ["*"] = { 'tmux', 'load-buffer', '-w', '-' },
+    },
+    paste = {
+      ["+"] = { 'bash', '-c', 'tmux refresh-client -l && sleep 0.2 && tmux save-buffer -' },
+      ["*"] = { 'bash', '-c', 'tmux refresh-client -l && sleep 0.2 && tmux save-buffer -' },
+    },
+    cache_enabled = false,
   }
 end
 
@@ -355,7 +435,8 @@ vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc =
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'bash', 'python', 'lua', 'go', 'rust', 'toml', 'yaml', 'json', 'javascript', 'typescript', 'help', 'vim',
+  ensure_installed = { 'c', 'cpp', 'bash', 'python', 'lua', 'go', 'rust', 'toml', 'yaml', 'json', 'javascript',
+    'typescript', 'help', 'vim',
     'markdown' },
   sync_install = false, -- Install parsers synchronously?
   auto_install = true, -- Automatically install missing parsers when entering buffer
@@ -484,12 +565,12 @@ local servers = {
   rust_analyzer = {},
   -- tsserver = {},
 
-  sumneko_lua = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-    },
-  },
+  -- sumneko_lua = {
+  --   Lua = {
+  --     workspace = { checkThirdParty = false },
+  --     telemetry = { enable = false },
+  --   },
+  -- },
 }
 
 -- Setup neovim lua configuration
